e4e8e506cc5f884c98db4bc54b2469b1a5931031
yuliy <y.globina@yandex.ru>
2023-02-04 11:25:49 +0300

    Initial commit

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,44 @@
+package ru.itmentor.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.itmentor.spring.boot_security.demo.model.User;
+import ru.itmentor.spring.boot_security.demo.service.UserService;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.userService = service;
+    }
+
+    @RequestMapping("/")
+    public String formroot() {
+        return ("/login");
+    }
+
+
+    @RequestMapping("/admin")
+    public String index(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
+        return "admin";
+    }
+
+    @RequestMapping("/user")
+    public String user(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        User user = (User) userService.loadUserByUsername(name);
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+
+}
